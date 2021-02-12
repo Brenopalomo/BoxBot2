649b3efa5cc3d0dea6c1645dd6044261d57dc5e1
Brenopalomo <76840613+Brenopalomo@users.noreply.github.com>
2021-02-12 13:44:16 -0300

    index.js

--- index.js
+++ index.js
@@ -131,7 +131,7 @@ async function starts() {
 			}
 
 			const botNumber = client.user.jid
-			const ownerNumber = ["wa.me/+5516999916885"] // replace this with your number
+			const ownerNumber = ["5516999916885@s.whatsapp.net"] // replace this with your number
 			const isGroup = from.endsWith('@g.us')
 			const sender = isGroup ? mek.participant : mek.key.remoteJid
 			const groupMetadata = isGroup ? await client.groupMetadata(from) : ''
